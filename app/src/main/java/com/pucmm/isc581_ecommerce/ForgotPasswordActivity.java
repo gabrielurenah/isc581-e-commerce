2f628ef34c3ddb0ea80e935c9575e8ddd793e2a4
gabriel urena <gabriel.uh@hotmail.com>
2020-11-09 13:23:26 -0400

    connects activities

--- app/src/main/java/com/pucmm/isc581_ecommerce/ForgotPasswordActivity.java
+++ app/src/main/java/com/pucmm/isc581_ecommerce/ForgotPasswordActivity.java
@@ -2,13 +2,30 @@ package com.pucmm.isc581_ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private TextView mLoginNow;
+    private TextView mRegisterNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        mLoginNow = findViewById(R.id.forgot_password_login_now);
+        mRegisterNow = findViewById(R.id.forgot_password_register_now);
+
+        mLoginNow.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        mRegisterNow.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterUserActivity.class));
+        });
+
     }
 }
